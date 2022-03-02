45b85ef45a1ea22cafa19818f7c3d75ee976364b
kleitonalbuquerque <kleiton2102@gmail.com>
2022-03-01 21:24:57 -0300

    Validação

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,6 +3,24 @@ class UserController {
 
   async create(req, res) {
     console.log(req.body);
+    var { email, name, password } = req.body;
+
+    if (email === undefined || email == "") {
+      res.status(400);
+      res.json({ err: "O e-mail é iválido!" });
+    }
+
+    if (password === undefined || password == "") {
+      res.status(400);
+      res.json({ err: "Senha é obrigatória!" });
+    }
+
+    if (password.length < 5) {
+      res.status(400);
+      res.json({ err: "A senha deve conter mais que cinco caracteres!" });
+    }
+
+    res.status = 200;
     res.send("Pegando o corpo da requisição!");
   }
 }
