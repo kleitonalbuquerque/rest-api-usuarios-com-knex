e44058d22bde3788670e05c8b6022c8c91a8d522
kleitonalbuquerque <kleiton2102@gmail.com>
2022-03-02 15:45:39 -0300

    Cadastrando usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,24 +5,35 @@ class UserController {
 
   async create(req, res) {
     console.log(req.body);
-    var { email, name, password } = req.body;
+    var { email, name, password, role } = req.body;
 
     if (email === undefined || email == "") {
       res.status(400);
       res.json({ err: "O e-mail é iválido!" });
+      return;
     }
 
     if (password === undefined || password == "") {
       res.status(400);
       res.json({ err: "Senha é obrigatória!" });
+      return;
     }
 
     if (password.length < 5) {
       res.status(400);
       res.json({ err: "A senha deve conter mais que cinco caracteres!" });
+      return;
     }
 
-    await User.new(name, email, password);
+    let emailExists = await User.findEmail(email);
+
+    if (emailExists) {
+      res.status(406);
+      res.json({ err: "O e-mail já está cadasttrado!" });
+      return;
+    }
+
+    await User.new(name, email, password, role); // await: só vai para a próxima linha qd essa task for executada
 
     res.status = 200;
     res.send("Pegando o corpo da requisição!");
--- models/User.js
+++ models/User.js
@@ -3,12 +3,29 @@ var db = require("../database/connection");
 var bcrypt = require("bcrypt");
 
 class User {
-  async new(name, email, password) {
+  async new(name, email, password, role) {
     try {
-      // await knex.insert({ name, email, password, role: 0 }).table("users");
-      await db("users").insert({ name, email, password });
+      let hash = await bcrypt.hash(password, 10);
+
+      await db("users").insert({ name, email, password: hash, role });
     } catch (err) {
       console.log(err);
+    }
+  }
+
+  async findEmail(email) {
+    try {
+      let result = await db.select("*").from("users").where({ email: email });
+      console.log(result);
+
+      if (result.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
     }
   }
 }
